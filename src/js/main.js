5d337d329f2e4e3c0f0ba3edff7579c9cb0ca200
arik912 <arik912@gmail.com>
2018-12-10 11:01:00 +0200

    sushi sale

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+
+
+/*
+ * Custom
+ */
+//= partials/app.js
+
+
+
+
+/*
+ * slick
+ */
+//= ../slick/slick.min.js
+
+
+$(document).ready(function(){
+      $('.slider').slick();
+    });
